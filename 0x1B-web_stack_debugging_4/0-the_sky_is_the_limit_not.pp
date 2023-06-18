8ae6d6ca887b03ccf825e6e3b84547dff4814824
basanya ajibola <albajd@gmail.com>
2023-06-18 19:55:42 +0100

    task

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
-Nginx server can handle
+# This manuscript increases the amount of traffic an Nginx server can handle
 
 exec { 'ngix-conf':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
@@ -9,4 +9,4 @@ exec { 'ngix-conf':
 -> exec { 'nginx-restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'
-}
+}}
